dcaa2c8337929f03d634f4a61597083abbbea1fc
flamingduck <sanderhoremans66@gmail.com>
2018-07-01 01:43:56 +0200

    making token private

--- main.js
+++ main.js
@@ -10,6 +10,3 @@ bot.login(config.token);
 bot.registry.registerGroup('random','Random');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "\\commands");
-
-var recource = require('./recources/commands/magic8ball.json');
-console.log(recource
