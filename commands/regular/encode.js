3fb5c734bbf90f68d6b771f5b4a40adcb556530d
flamingduck <sanderhoremans66@gmail.com>
2018-07-14 20:17:29 +0200

    adding  basic encode

---
+++ commands/regular/encode.js
@@ -0,0 +1,35 @@
+const Commando = require('discord.js-commando');
+
+class EncodeCommand extends Commando.Command {
+	constructor(client) {
+		super(client, {
+			name: 'encode',
+			group: 'regular',
+			memberName: 'encode',
+			description: 'Replies with the text you provide.',
+			examples: ['say Hi there!'],
+			args: [
+			{
+				key: 'text',
+				prompt: 'What text would you like the bot to say?',
+				type: 'string'
+			}
+			]
+		});
+	}
+
+	async run(msg, { text }) {
+		msg.delete();
+		text = text.replace(/[a-zA-Z]/g, function(temp) {
+			if (temp === 'z') return 'a';
+			else if (temp === 'Z') return 'A';
+			else return String.fromCharCode(temp.charCodeAt(0) + 1);
+		});
+		return text.replace(/[aeiou]/g, function(temp) {
+			return temp.toUpperCase();
+		});
+
+		return msg.say(encodedText);
+	}
+};
+module.exports = EncodeCommand;
